5e100499f40b715d5f12b04a0193082ea9cae05b
Mickael <volamigaming13@gmail.com>
2022-09-21 17:58:42 +0200

    Faire la méthode comme prof veux

--- src/Fibo.java
+++ src/Fibo.java
@@ -41,7 +41,7 @@ public class Fibo {
     }
 
 
-    public static double[] SuiteExpoMatrice(int n){
+    /*public static double[] SuiteExpoMatrice(int n){
         double[][] A = new double[2][2];
         A[0][0] = 1;
         A[0][1] = 1;
@@ -60,7 +60,47 @@ public class Fibo {
             V = next;
         }
         return V;
+    }*/
+
+    static boolean isPowerOfTwo(int n)
+    {
+        if (n == 0)
+            return false;
+
+        while (n != 1) {
+            if (n % 2 != 0)
+                return false;
+            n = n / 2;
+        }
+        return true;
     }
+    public static double[] SuiteExpoMatrice(int n){
+        double[][] A = new double[2][2];
+        A[0][0] = 1;
+        A[0][1] = 1;
+        A[1][0] = 1;
+        A[1][1] = 0;
+
+        double[] V = new double[2];
+        V[0] = 1;
+        V[1] = 0;
+
+        if(isPowerOfTwo(n)) {
+
+        }
+
+        if(n == 0) System.out.println(V[1]);
+        else if(n == 1) System.out.println(V[0]);
+
+
+
+        for(int i = 2; i <= n; i++){
+            double[] next = multiply(A, V);
+            V = next;
+        }
+        return V;
+    }
+
 
     // Methode multiply reprise d'Internet
     public static double[] multiply(double[][] matrix, double[] vector) {
