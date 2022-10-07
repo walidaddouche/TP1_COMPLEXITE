317703864a67c8945330ce0ce02f9dd13cd4df9f
ADDOUCHE Walid <73178026+walidaddouche@users.noreply.github.com>
2022-10-07 16:02:22 +0200

    Qst 3 exo 2 done , Last push

--- src/Exo2/Graph.java
+++ src/Exo2/Graph.java
@@ -4,13 +4,20 @@ import java.util.*;
 
 public class Graph {
     Utils utils = new Utils();
-    int size ;
+    int size;
 
     public int[][] matrix; // Adjacency matrix == matrix d'adjacence
 
+    boolean[] testedElements;
+
+    List<Integer> voidMax = new ArrayList<>();
+
+    ArrayList<Integer> voidTemp = new ArrayList<>();
+
+
     public Graph(int[][] matrix) {
         this.matrix = matrix;
-        this.size   = this.matrix.length;
+        this.size = this.matrix.length;
         //array[i][j] = 1;
     }
 
@@ -42,26 +49,51 @@ public class Graph {
         return X.length != 0;
     }
 
-    public  boolean IsConnected(List<Integer> list, int s){
+    public boolean IsConnected(List<Integer> list, int s) {
         //
         // on verifie si entre une list de sommet et un sommet y'a pas de lien 
         // si il y'a un lien on retourne false sinon true
-
-        for(int x: list){
-            if(matrix[s][x] == 1) return false;
+        for (int x : list) {
+            if (matrix[s][x] == 1) return false;
         }
         return true;
     }
 
     // Question 2
-    public List<Integer> EmptyZoneMaximal(){
-            List<Integer> result = new ArrayList<>();
-            for(int i =0; i< matrix.length; i++){
-                if(IsConnected(result, i)) {
-                    result.add(i);
-                }
+    public List<Integer> EmptyZoneMaximal() {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < matrix.length; i++) {
+            if (IsConnected(result, i)) {
+                result.add(i);
             }
-            return result;
+        }
+        return result;
+    }
+
+   //Methodes realisé avec un camarade
+    public void getAllMaximalVoids(int level) {
+        if (level >= size) {
+            if (voidTemp.size() > voidMax.size()) {
+                voidMax = (List<Integer>) voidTemp.clone();
+            }
+            return;
+        }
+        for (int i = 0; i < size; i++) {
+            if (!testedElements[i]) {
+                testedElements[i] = true;
+                if (IsConnected(voidTemp, i)) voidTemp.add(i);
+                getAllMaximalVoids(level + 1);
+                testedElements[i] = false;
+                voidTemp.remove(Integer.valueOf(i));
+            }
+        }
+    }
+
+    // QUESTION 3
+    public List<Integer> MaximumEmptyZone() {
+        testedElements = new boolean[size];
+        getAllMaximalVoids(0);
+        return voidMax;
     }
 
 
@@ -70,25 +102,23 @@ public class Graph {
 
         Graph graph = new Graph(matrix);
 
-        graph.addArc(0, 1);
-        graph.addArc(0, 5);
-        graph.addArc(1, 2);
-        graph.addArc(1, 3);
-        graph.addArc(1, 7);
+        graph.addArc(0, 1); graph.addArc(0, 5);
+        graph.addArc(1, 2); graph.addArc(1, 3); graph.addArc(1, 7);
         graph.addArc(2, 4);
         graph.addArc(3, 6);
         graph.addArc(4, 6);
-        graph.addArc(5, 8);
-        graph.addArc(5, 7);
-        graph.addArc(6, 7);
-        graph.addArc(6, 8);
+        graph.addArc(5, 8);graph.addArc(5, 7);
+        graph.addArc(6, 7);graph.addArc(6, 8);
 
-      //  int[] list = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8};
+        //  int[] list = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8};
 
         //
-       // System.out.println(graph.EmptyZoneMaximal());
-        List<Integer> list = List.of(5);
+        // System.out.println(graph.EmptyZoneMaximal());
+        /*List<Integer> list = List.of(5);
         System.out.println(graph.IsConnected(list, 4));
+
+         */
+        System.out.println(graph.MaximumEmptyZone());
     }
 
 
